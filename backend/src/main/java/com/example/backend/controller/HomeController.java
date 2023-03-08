b5c53e9ca538fa7915a2f0330995f1aed01fd964
Boyi Qu <boyiqu9596@gmail.com>
2023-03-07 21:51:12 -0800

    added backend

---
+++ backend/src/main/java/com/example/backend/controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.example.backend.controller;
+
+import com.example.backend.models.House;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class HomeController {
+    private List<House> houses = new ArrayList<>();
+
+
+}
