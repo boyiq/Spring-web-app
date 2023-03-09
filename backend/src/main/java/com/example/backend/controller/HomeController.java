d372f6af5e277fd5e6249152676514500972cf15
Boyi Qu <boyiqu9596@gmail.com>
2023-03-08 23:09:25 -0800

    added get post api endpoints

--- backend/src/main/java/com/example/backend/controller/HomeController.java
+++ backend/src/main/java/com/example/backend/controller/HomeController.java
@@ -1,6 +1,11 @@
 package com.example.backend.controller;
 
 import com.example.backend.models.House;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -9,6 +14,21 @@ import java.util.List;
 @RestController
 public class HomeController {
     private List<House> houses = new ArrayList<>();
+
+    @GetMapping("/city")
+    public List<String> getCity() {
+        List<String> cityList = new ArrayList<>();
+        for (int i = 0; i < houses.size(); i ++) {
+            cityList.add(houses.get(i).getCity());
+        }
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<Void> create(@RequestBody House newHouse) {
+        houses.add(newHouse);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
 
 
 }
