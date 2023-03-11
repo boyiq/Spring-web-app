34e599cdae534bc2110fd56fb6080fe4ac209db1
Boyi Qu <boyiqu9596@gmail.com>
2023-03-10 23:24:41 -0800

    modified homeController

--- backend/src/main/java/com/example/backend/controller/HomeController.java
+++ backend/src/main/java/com/example/backend/controller/HomeController.java
@@ -1,8 +1,6 @@
 package com.example.backend.controller;
 
 import com.example.backend.models.House;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
