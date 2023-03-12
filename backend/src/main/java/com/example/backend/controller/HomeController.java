e766120347c9980a46ca6971bdd3bf2d1e845e7f
Boyi Qu <boyiqu9596@gmail.com>
2023-03-11 23:28:41 -0800

    modified homecontroller

--- backend/src/main/java/com/example/backend/controller/HomeController.java
+++ backend/src/main/java/com/example/backend/controller/HomeController.java
@@ -1,6 +1,8 @@
 package com.example.backend.controller;
 
-import com.example.backend.models.House;
+import com.example.backend.models.Sales;
+import com.example.backend.repository.SaleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,24 +13,15 @@ import java.util.List;
 
 @RestController
 public class HomeController {
-    private List<House> houses = new ArrayList<>();
+
+    @Autowired
+    private SaleRepository salesRepository;
 
     @GetMapping("/sales")
     public String getAllSales () {
         return "Here are all sales";
     }
-//    public List<String> getCity() {
-//        List<String> cityList = new ArrayList<>();
-//        for (int i = 0; i < houses.size(); i ++) {
-//            cityList.add(houses.get(i).getCity());
-//        }
-//    }
 
-//    @PostMapping("/addsale")
-//    public ResponseEntity<Void> create(@RequestBody House newHouse) {
-//        houses.add(newHouse);
-//        return ResponseEntity.status(HttpStatus.CREATED).build();
-//    }
 
 
 
