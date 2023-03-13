687b2348ff093debdb1027d02716c8ad0a9f11aa
Boyi Qu <boyiqu9596@gmail.com>
2023-03-12 18:52:34 -0700

    added get/sales endpoint to filter sales by criteria

--- backend/src/main/java/com/example/backend/controller/HomeController.java
+++ backend/src/main/java/com/example/backend/controller/HomeController.java
@@ -1,12 +1,11 @@
 package com.example.backend.controller;
 
-import com.example.backend.models.Sales;
+import com.example.backend.models.Sale;
 import com.example.backend.repository.SaleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,11 +14,40 @@ import java.util.List;
 public class HomeController {
 
     @Autowired
-    private SaleRepository salesRepository;
+    private SaleRepository saleRepository;
 
-    @GetMapping("/sales")
-    public Iterable<Sales> getAllSales () {
-        Iterable<Sales> salesList = salesRepository.findAll();
+    @GetMapping("/allsales")
+    public Iterable<Sale> getAllSales () {
+        Iterable<Sale> salesList = saleRepository.findAll();
         return salesList;
     }
+
+    @GetMapping("/sales")
+    public ResponseEntity<List<Sale>> getSalesByCriteria (
+            @RequestParam(name = "zipCode", required = false) String zipCode,
+            @RequestParam(name = "minPrice", required = false) Integer minPrice,
+            @RequestParam(name = "maxPrice", required = false) Integer maxPrice,
+            @RequestParam(name = "minBeds", required = false) Integer minBeds,
+            @RequestParam(name = "maxBeds", required = false) Integer maxBeds,
+            @RequestParam(name = "minBaths", required = false) Integer minBaths,
+            @RequestParam(name = "maxBaths", required = false) Integer maxBaths,
+            @RequestParam(name = "minSize", required = false) Integer minSize,
+            @RequestParam(name = "maxSize", required = false) Integer maxSize
+    ) {
+        List<Sale> salesList = saleRepository.findAllByCriteria(
+                zipCode, minPrice, maxPrice, minBeds, maxBeds, minBaths, maxBaths, minSize, maxSize);
+        return new ResponseEntity<>(salesList, HttpStatus.OK);
+    }
+
+    @PostMapping("/addsale")
+    public ResponseEntity<String> addSale(@RequestBody Sale sale) {
+        try {
+            saleRepository.save(sale);
+            return new ResponseEntity<>("House sale record added", HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>("Failed to add house sale" + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+
 }
