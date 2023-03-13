d303f2f2ec4998da89e923832204850ee6340cd7
Boyi Qu <boyiqu9596@gmail.com>
2023-03-12 18:54:10 -0700

    added findAllByCriteria method to sale repository

--- backend/src/main/java/com/example/backend/repository/SaleRepository.java
+++ backend/src/main/java/com/example/backend/repository/SaleRepository.java
@@ -1,12 +1,25 @@
 package com.example.backend.repository;
 
-import com.example.backend.models.Sales;
+import com.example.backend.models.Sale;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface SaleRepository extends CrudRepository <Sales, Integer>{
+public interface SaleRepository extends CrudRepository <Sale, Integer>{
+    @Query("SELECT hs FROM Sale hs WHERE (:targetZipCode is null or hs.zipCode = :targetZipCode) AND hs.price BETWEEN :minPrice AND :maxPrice AND hs.beds BETWEEN :minBeds and :maxBeds AND hs.baths BETWEEN :minBaths and :maxBaths AND hs.size BETWEEN :minSize and :maxSize")
+    List<Sale> findAllByCriteria(
+            @Param("targetZipCode") String targetZipCode,
+            @Param("minPrice") Integer minPrice,
+            @Param("maxPrice") Integer maxPrice,
+            @Param("minBeds") Integer minBeds,
+            @Param("maxBeds") Integer maxBeds,
+            @Param("minBaths") Integer minBaths,
+            @Param("maxBaths") Integer maxBaths,
+            @Param("minSize") Integer minSize,
+            @Param("maxSize") Integer maxSize);
 
 }
